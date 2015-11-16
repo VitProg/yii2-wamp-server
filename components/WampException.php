f6cc44882cb0f8dae0ee3d50cbf5fcc536013721
VitProg <vitprog@gmail.com>
2015-11-17 00:47:54 +0300

    add beforeCall, afterCall and WampExeption

---
+++ components/WampException.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VitProg
+ * Date: 17.11.2015
+ * Time: 0:37
+ */
+
+namespace vitprog\wamp\components;
+
+
+use Exception;
+
+class WampException extends \Exception {
+
+    public $method;
+
+    /**
+     * WampException constructor.
+     * @param string $message
+     * @param int $code
+     * @param string $method
+     * @param Exception $previous
+     */
+    public function __construct($message, $code, $method, Exception $previous) {
+        parent::__construct($message, $code, $previous);
+        $this->method = $method;
+    }
+
+
+    public function toArray() {
+        $data = [
+            'result' => 'error',
+            'code' => $this->code,
+        ];
+        if (defined('WAMP_DEBUG') && WAMP_DEBUG == true) {
+            $data['file'] = $this->file;
+            $data['line'] = $this->line;
+            $data['method'] = $this->method;
+        }
+        return $data;
+    }
+
+}
