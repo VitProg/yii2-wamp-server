197f9943ae82b8532ba149d5c7ea850da3252dac
VitProg <vitprog@gmail.com>
2015-12-28 14:35:34 +0300

    add WampResult

---
+++ components/WampRequest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VitProg
+ * Date: 28.12.2015
+ * Time: 14:33
+ */
+
+namespace vitprog\wamp\components;
+
+
+class WampRequest extends \yii\console\Request {
+
+
+    public function getUrl() {
+        return '';
+    }
+
+}
